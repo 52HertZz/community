876c93d0af3e1ec16b365afa719756d257ce0554
52HertZz <lh141x@outlook.com>
2019-12-09 20:43:48 +0800

    引入MyBatis Generator，重新生成Model，mapper和对映的mapper.xml映射文件

--- src/main/java/com/hnisc/community/CommunityApplication.java
+++ src/main/java/com/hnisc/community/CommunityApplication.java
@@ -1,8 +1,11 @@
 package com.hnisc.community;
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hnisc.community.mapper")
 public class CommunityApplication {
 
     public static void main(String[] args) {
